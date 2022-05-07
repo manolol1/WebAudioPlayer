43efb68bff409827b6c2cd9fcf568c803f406479
Manolol <manolol@mailfence.com>
2022-05-07 11:33:49 +0200

    Added webserver
    More details how to use the webserver in README.md

---
+++ index.js
@@ -0,0 +1,18 @@
+// Configuration
+var serverPort = 2225; // If you want, you can change the port for the http server. Default = 2225
+
+console.log("Initialising express webserver...")
+const express = require("express")
+const fs = require('fs');
+const http = require('http');
+
+let app = express();
+
+app.get("/", (req, res) => {
+    var website = fs.readFileSync(__dirname + "/webaudioplayer.html").toString();
+    console.log("Sent index.html to a client");
+    res.status(200).send(website);
+});
+
+var httpServer = http.createServer(app);
+httpServer.listen(serverPort, () => console.log("WebAudioPlayer is running on port" + serverPort));
