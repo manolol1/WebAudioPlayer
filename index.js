285d940260669c70918768e1bee1d39f4994fb8e
Manolol <manolol@mailfence.com>
2022-05-09 20:34:46 +0200

    Commented the code of the webserver
    ... to make it a bit more understandable.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Configuration
 const serverPort = 2225; // If you want, you can change the port for the http server. Default = 2225
 
+// Importing stuff
 console.log("Initialising express webserver...")
 const express = require("express")
 const fs = require('fs');
@@ -8,10 +9,12 @@ const http = require('http');
 
 const app = express();
 
+// route for the website
 app.get("/", (req, res) => {
     const website = fs.readFileSync(__dirname + "/webaudioplayer.html").toString();
     res.status(200).send(website);
 });
 
+// Creating webserver
 const httpServer = http.createServer(app);
 httpServer.listen(serverPort, () => console.log("WebAudioPlayer is running on port" + serverPort));
